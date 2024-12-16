fed6fa7fa1cf2749cf919d040fa79ad7681e846b
Eren Yalçın <yalcineren97@gmail.com>
2024-12-16 23:22:58 +0100

    setup venv

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 """Chat bot module"""
-
 import os
 
 from binance.um_futures import UMFutures
@@ -14,10 +13,43 @@ load_dotenv()
 BINANCE_API_KEY = os.getenv("BINANCE_API_KEY")
 BINANCE_SECRET_KEY = os.getenv("BINANCE_SECRET_KEY")
 
+print(BINANCE_API_KEY)
+print(BINANCE_SECRET_KEY)
+
 client = UMFutures(key=BINANCE_API_KEY, secret=BINANCE_SECRET_KEY)
 
-basicUtils = BasicUtility(client)
-candleUtils = CandleUtility(client)
+# basicUtils = BasicUtility(client)
+# candleUtils = CandleUtility(client)
 
-pr(basicUtils.get_position("BTCDOMUSDT"))
-pr(candleUtils.get_candles("BTCUSDT", "1m", 5))
+# print(basicUtils.get_balance("USDT"))
+
+
+# pr(basicUtils.get_position("BTCDOMUSDT"))
+# pr(candleUtils.get_candles("BTCUSDT", "1m", 5))
+
+
+try:
+    # Set margin type to ISOLATED for DOGEUSDT
+    client.change_margin_type(symbol='DOGEUSDT', marginType='ISOLATED')
+except Exception as e:
+    print(f"Error changing margin type: {e}")
+
+
+# Set leverage to 2x for DOGEUSDT
+client.change_leverage(symbol='DOGEUSDT', leverage=2)
+
+# Get current price of DOGEUSDT
+price = float(client.ticker_price(symbol='DOGEUSDT')['price'])
+
+# Calculate quantity to buy with 5 USDT
+quantity = round(8 / price, 0)  # Adjust quantity to required precision
+
+# Place a market order to buy DOGEUSDT
+order = client.new_order(
+    symbol='DOGEUSDT',
+    side='BUY',
+    type='MARKET',
+    quantity=quantity
+)
+
+print(order)
